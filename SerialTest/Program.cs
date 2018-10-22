273025f0fb22b1d1c93c534f6205d2eba6521230
Italo Siqueira <siqueira.italo@gmail.com>
2018-10-22 17:27:37 -0300

    Add files

--- SerialTest/Program.cs
+++ SerialTest/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO.Ports;
 
 namespace SerialTest
 {
@@ -6,7 +7,13 @@ namespace SerialTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Serial ports available:");
+            Console.WriteLine("-----------------------");
+            foreach (var portName in SerialPort.GetPortNames())
+            {
+                Console.WriteLine(portName);
+            }
+            Console.ReadLine();
         }
     }
 }
